00b15770acc6e886016ac4509e561f96ff279b9b
leisonator <71030436+leisonator@users.noreply.github.com>
2022-10-28 22:04:50 -0600

    Hackathon promt should automatically center to facebook status update on a 1440 screen

---
+++ HACKATHON.py
@@ -0,0 +1,9 @@
+import time
+import pyautogui
+time.sleep(5)
+pyautogui.moveTo(2559, 700, duration = 2)
+pyautogui.moveTo(1300, 510, duration = 2)
+pyautogui.leftClick (1300, 510)
+pyautogui.typewrite("Player Has Died, Game Over Happy Halloween")
+time.sleep(1)
+pyautogui.moveTo(1300, 850, duration = 2)
---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+import pyautogui
+time.sleep(5)
+pyautogui.moveTo(1300, 510, duration = 2)
+pyautogui.leftClick (1300, 510)
+pyautogui.typewrite("Player Has Died, Game Over Happy Halloween")
+time.sleep(1)
+pyautogui.moveTo(1300, 940, duration = 2)
